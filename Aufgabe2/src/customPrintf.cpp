bbfdd4183868d41d3bfa328530b97b2bb83371be
BruegelN <BruegelN@users.noreply.github.com>
2016-04-03 22:24:45 +0200

    started to differentiate the format to print.

--- Aufgabe2/src/customPrintf.cpp
+++ Aufgabe2/src/customPrintf.cpp
@@ -1,1 +1,38 @@
 #include "customPrintf.h"
+
+char* Output::Printf(char *dst, const void *end, const char *fmt, ...)
+{
+  if(dst == nullptr || fmt == nullptr || fmt[0] != '%')
+  {
+    // invalid call
+    return nullptr;
+  }
+
+
+  if('d' == fmt[1]) // %d signed int
+  {
+
+  }
+  else if('u' == fmt[1]) // %u unsigned int
+  {
+
+  }
+  else if('c' == fmt[1]) // %c char
+  {
+
+  }
+  else if('s' == fmt[1]) // %s char[]
+  {
+
+  }
+  else if('x' == fmt[1]) // %x 0x16
+  {
+
+  }
+  else if('b' == fmt[1]) // %b b'110'
+  {
+
+  }
+
+  return nullptr;
+}
--- Aufgabe2/src/customPrintf.h
+++ Aufgabe2/src/customPrintf.h
@@ -1,4 +1,19 @@
 #ifndef CUSTOM_PRINTF_H
   #define CUSTOM_PRINTF_H
 
-#endif /* CUSTOM_PRINTF_H */
+/*
+TODO
+%d fuer signed int
+%u fuer  unsigned int
+%c fue ein einzelnes Zeichen
+%s fuer eine Zeichenkette
+%x fuer die Ausgabe in Hexadezimalform
+%b fuer die Ausgabe in Binaerform
+*/
+
+namespace Output
+{
+  char* Printf(char* dst, const void* end, const char* fmt, ...);
+}
+
+#endif // CUSTOM_PRINTF_H
