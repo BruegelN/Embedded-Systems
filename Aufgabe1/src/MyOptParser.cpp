2a9c350bcc4112ed8ac107a00820ead9f729e40c
BruegelN <BruegelN@users.noreply.github.com>
2016-04-04 21:43:42 +0200

    Removed silly comment

--- Aufgabe1/src/MyOptParser.cpp
+++ Aufgabe1/src/MyOptParser.cpp
@@ -9,9 +9,9 @@ MyOptParser::MyOptParser()
 
 bool MyOptParser::option(char c, const char* info)
 {
-  if((c >= '0' && c <= '9' )||  // the value has to be in range 0-9
-    (c >= 'A' && c <= 'Z' ) ||  // or in range A-Z
-    (c >= 'a' && c <= 'z'))    // or in range a-z
+  if((c >= '0' && c <= '9' )||
+    (c >= 'A' && c <= 'Z' ) ||
+    (c >= 'a' && c <= 'z'))
   {
     std::cout << "\tArgument: " << c << " given." << std::endl;
     if(nullptr != info)
