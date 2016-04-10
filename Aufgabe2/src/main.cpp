77babd28bcc0d3381a588ca3fb3c73d64359d9b6
BruegelN <BruegelN@users.noreply.github.com>
2016-04-10 17:33:53 +0200

    added minimal test in main()

--- Aufgabe2/src/main.cpp
+++ Aufgabe2/src/main.cpp
@@ -1,7 +1,21 @@
+#include <iostream>
+
 #include "Printf.h"
+
+#define BUFFER_SIZE 50
 
 int main(void)
 {
+  char buffer[BUFFER_SIZE] = {0};
+  Printf(buffer, buffer+sizeof(buffer), "%sXXX%dXXX", "123", 123 );
+
+
+  std::cout << "BUFFER: " << std::endl;
+  for(int i = 0;  i < BUFFER_SIZE; i++)
+  {
+    std::cout << i << " is " << buffer[i] << std::endl;
+  }
+
   return 0;
 
 }
