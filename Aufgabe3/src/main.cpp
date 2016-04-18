8588859a413fc8e85f34071f16c5be15a1b066c7
BruegelN <BruegelN@users.noreply.github.com>
2016-04-18 18:03:13 +0200

    Added some code for testing.

--- Aufgabe3/src/main.cpp
+++ Aufgabe3/src/main.cpp
@@ -1,9 +1,24 @@
+#include <iostream>
+
 #include "PreAllocString.h"
 
 
 int main(void) {
 
-  PreAllocString<20> walloHelt;
+  PreAllocString<20> test;
+  test = "test";
+  test += '3';
+  test.AddWhiteSpace();
+  test += "adsf";
+  test += '3';
+
+  size_t lenght = test.GetLength();
+
+  std::cout << "Strlen is: " << lenght << std::endl;
+  for(size_t i = 0;  i < lenght; i++)
+  {
+    std::cout << i << " is " << test[i] << std::endl;
+  }
 
   return 0;
 }
