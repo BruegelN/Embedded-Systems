75a2dfdc4e21f377540d62ffe0c95180805ef7b4
BruegelN <BruegelN@users.noreply.github.com>
2016-04-26 22:30:57 +0200

    A basic branching in main either server or client mode in main.cpp.

--- Aufgabe5/src/main.cpp
+++ Aufgabe5/src/main.cpp
@@ -8,9 +8,22 @@ int main(int argc, const char** argv)
   SocketOptParser theParser = SocketOptParser();
   if(!theParser.parse(argc, argv))
   {
-    // TODO return with help text
+    // wrong usage, print help and exit
     std::cout << "Help for " << argv[0] << ":" << std::endl;
     std::cout << theParser.getHelpText() << std::endl;
+    return 0;
+  }
+
+  if(theParser.isServerMode())
+  {
+    // perform server task
+    
+
+  }
+  else
+  {
+    // running as client
+
   }
   return 0;
 }
